2ad9318cebfea08a428e79383747139aad90234d
Nathaniel Knopf <nathanielknopf@gmail.com>
2014-03-14 16:33:51 -0700

    First push - code not currently usable, V_0.2.0b to be finished in near future.

---
+++ newParsing.py
@@ -0,0 +1,198 @@
+# 03/14/2014 - V_0.2.0b
+# This script parses raw data created by TubeCode.py and WheelCode.py for use with CLOCKLAB.
+#
+# IN THIS VERSION:
+# * Spared user from having to open code by implementing new functions for collecting necessary
+#   data from user modified text file.
+# * Eliminated errors with inconsistencies in parsed data.
+# * Heavily commented to maxamize comprehensibility and to minimize ambiguity in interpreting
+#   methodology/functionality.
+#
+# For a highly-detailed description of the methodology implemented by this script, check README.txt
+# 
+# If this code works, it was written by Nathaniel Knopf
+# If it doesn't, I have no idea who wrote it.
+#
+# TODO:
+# MAKE TODO LIST
+
+# Required Libraries:
+import decimal as dc
+import time
+from os import system
+import sys
+
+# A counter which never gets reset - Counts total revolutions of the wheel:
+# PLEASE ANNOTATE WHICH FUNCTIONALITY THIS COUNTER SERVES:
+# [X] - TOTAL TIMES WHEEL PHYSICALLY TURNS (INDEPENDENT OF ANIMAL COUNTS)
+# [ ] - TOTAL OF ALL REVOLUTIONS FOR ANIMALS (DEPENDENS ON ANIMAL COUNTS)
+totalRevolutions = 0.0
+
+months = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 
+                        'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
+
+# A function which waits while information is displayed on the screen
+# Displays time remaining in wait period on the last line displayed in Terminal
+def wait(text, seconds):
+	while seconds > 0:
+		toPrint = text + str(seconds)
+		sys.stdout.write('%s\r' % toPrint)
+		sys.stdout.flush()
+		time.sleep(1)
+		seconds -= 1
+
+# These two functions find the date and the time in the preferred format for CLOCKLAB
+def findAscDate(timeEpoch):
+        ascRaw = time.ctime(timeEpoch)
+        ascDate = ascRaw[-2:] + '/' +  months[ascRaw[4:7]] + '/' + ascRaw[8:10]
+        return ascDate 
+
+def findAscTime(timeEpoch):     
+        ascRaw = time.ctime(timeEpoch)
+        ascTime = ascRaw[11:19] + '.00'
+        return ascTime
+
+# For creating objects for each mouse --
+# In this version of the Parsing Code, each mouse has its own object, in which relevant
+# variables are stored. These include the tag, flags for the two gates, a flag for state
+# of in/out of the wheel, a counter for wheel revolutions attributed to the specific
+# mouse in the current block of time, and a counter for total revolutions attributed to the
+# specific mouse. The file to which parsed data is written is also stored as an object
+# within the mouse object.
+class Mouse:
+	# Called when an instance of Mouse class is created (i.e. new mouse object)
+	# Sets defaults to vars, as well as opens the file for parsed data
+	def __init__(self, inputTag):
+		self.tag = inputTag
+		self.gateOne = False
+		self.gateTwo = False
+		self.inWheel = False
+		self.ranThisBlock = 0.0
+		self.ranTotal = 0.0
+		self.file = self.makeFile()
+
+	# Makes a file for the mouse and writes headers to it for CLOCKLAB
+	def makeFile(self, self.tag):
+		filename = self.tag + '.txt'
+		mouseFile = open(filename, 'w')
+		mouseFile.write('GROUP ' + self.tag + '                                                                                 :\n\n---------\nUNIT TIME=\n')             
+	
+	# Switches values of Boolean Vars (for gates)
+	def switchValue(self, gate):
+		if gate == '1':
+			self.gateOne = not self.gateOne
+		elif gate == '2':
+			self.gateTwo = not self.gateTwo
+
+	# Counts a turn for the specific mouse in both the current block and in total
+	def countTurn(self):
+		self.ranThisBlock += 1
+		self.ranTotal += 1
+
+# Given the file in which configs are stored, pulls all needed data from config file.
+def getConfigs(fileName):
+	configs = []
+	configsFile = open(fileName, 'r')
+	configsFile.readline()
+	for line in configsFile:
+		configs.append(line[11:-1])
+	return configs
+
+# Called when newParsing.py is run - creates mouse objects and loads/stores configs
+def setup():
+	# Begins by loading configs
+	try:
+		configs = getConfigs('config.txt') # Default file for configs
+	except IOError:
+		# configs are not stored in configs.txt:
+		configsFileName = raw_input("Enter configs file: (Include the file extension) ")
+		configs = getConfigs(configsFileName)
+	except:
+		print "ERROR - FATAL: CONFIGS" # Error in collecting configs
+
+	# Configure mouseOne
+	print "Configuring MouseOne:"
+	global mouseOne
+	mouseOne = Mouse(configs[0])
+	print "Tag: " + mouseOne.tag + "\n"
+	
+	# Configure mouseTwo
+	print "Configuring MouseTwo:"
+	global mouseTwo
+	mouseTwo = Mouse(configs[1])
+	print "Tag: " + mouseTwo.tag + "\n"
+
+	# Configure mouseThree
+	print "Configuring MouseThree:"
+	global mouseThree
+	mouseThree = Mouse(configs[2])
+	print "Tag: " + mouseThree.tag + "\n"
+
+	# Configure mouseFour
+	print "Configuring MouseFour:"
+	global mouseFour
+	mouseFour = Mouse(configs[3])
+	print "Tag: " + mouseFour.tag + "\n"
+
+	# Open raw data from CSV file specified in configs.
+	global csvfile
+	csvFilename = configs[4]
+	if csvFilename[-4:] != '.csv':
+		csvFilename += '.csv'
+	print "CSV File: " + csvfile + "\n"
+	try:
+		csvfile = open(csvFilename, 'r')
+		print csvfile + " opened successfully."
+	except IOError:
+		# Could not find CSV File
+		print "ERROR - FILE DOES NOT EXIST IN LOCAL DIRECTORY"
+	except:
+		print "ERROR - FATAL ERROR: CSVFILE" # Error in finding raw data
+
+	# Interval - Length of block in seconds.
+	global interval
+	interval = float(configs[5])
+	print "Interval: " + str(interval) + "\n"
+
+	# Scale factor - necessary for CLOCKLAB as all values >=1000 are ignored.
+	# Results are scaled by a factor of 1/scale before being written to the CLOCKLAB files.
+	global scale
+	scale = float(configs[6])
+	print "Scale: " + str(scale) + "\n"
+
+	print "Beginning Parsing...\n"
+
+	wait(5)
+	system('clear')
+
+# This function checks for cases (typical and atypical) detailed in the README
+# Function takes a list of mouse objects as input - Accommodates any length list 
+# (i.e. any number of mice objects)
+def checkCases(mice):
+	# Iterate through all mice objects in the list of mice
+	for mouse in mice:
+		# CASE ONE - Gate One and Gate Two flags are both set to True
+		if (mouse.gateOne and mouse.gateTwo):
+			# RESULT - Mouse is in the wheel
+			mouse.inWheel = True
+		# CASE TWO - Gate One and Gate Two flags are both set to False
+		elif (not(mouse.gateOne) and not(mouse.gateTwo)):
+			# RESULT - Mouse is not in the wheel
+			mouse.inWheel = False
+		# CASE THREE - Gate One is False, Gate Two is True, and inWheel Flag is False
+		elif (not(mouse.gateOne) and mouse.gateTwo and not(mouse.inWheel)):
+			# RESULT - Mouse is in the wheel (ALSO: Set Gate One Flag to True)
+			mouse.inWheel = True
+			mouse.gateOne = True
+		# CASE FOUR - Gate One is False, Gate Two is True, and inWheel Flag is True
+		elif (not(mouse.gateOne) and mouse.gateTwo and mouse.inWheel):
+			# RESULT - Mouse is not in the wheel (ALSO: Set Gate Two Flag to False)
+			mouse.inWheel = False
+			mouse.gateTwo = False
+
+def main():
+	
+	system('clear')	
+	setup()
+
+main()
